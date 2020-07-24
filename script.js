01f60bee74e1aa70b2a0cc0aacdd34ec08c77f83
Matt Javier <matthewrjavier@gmail.com>
2020-07-24 10:18:55 -0700

    updates to timer, cleaner code

--- script.js
+++ script.js
@@ -2,22 +2,14 @@ let time = 100
 
 document.getElementById('counter').textContent = time
 
-let interval
-
-function startTimer() {
-  interval = setInterval(timer, 1000)
-}
-
-function timer() {
-  if (time <= 0) {
-    interval.clearInterval(interval);
-  }
-  time--
-  document.getElementById('counter').textContent = time
-}
-
 // start timer when startBtn clicked
 document.getElementById('startBtn').addEventListener('click', function() {
-  startTimer()
+  setInterval(function() {
+    if (time <= 0) {
+      clearInterval();
+    }
+    time--
+    document.getElementById('counter').textContent = time
+  }, 1000)
 })
 
