aaf5aa6811716b405d265aa6eedf68d29d9fcb2e
Matt Javier <matthewrjavier@gmail.com>
2020-07-24 00:03:32 -0700

    implemented timer when start button is clicked

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 let time = 100
+
+document.getElementById('counter').textContent = time
+
+
+// start timer when startBtn clicked
+document.getElementById('startBtn').addEventListener('click', setInterval(function() {
+  if (time <= 0) {
+    clearInterval();
+  }
+  time--
+  document.getElementById('counter').textContent = time
+}, 1000))
+
