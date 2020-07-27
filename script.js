b9f372363806919292a6710a92939d90599f3652
Matt Javier <matthewrjavier@gmail.com>
2020-07-27 10:25:26 -0700

    centering main content

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ let questions = [
     a2: 'booleans',
     a3: 'alerts',
     a4: 'numbers',
-    correct: 'alerts'
+    correct: 'alerts',
+    isCorrect: false
   },
   {
     question: 'The condition in an if / else statement is enclosed within _____.',
@@ -28,7 +29,8 @@ let questions = [
     a2: 'curly brackets',
     a3: 'parentheses',
     a4: 'square brackets',
-    correct: 'parantheses'
+    correct: 'parantheses',
+    isCorrect: false
   },
   {
     question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
@@ -36,7 +38,8 @@ let questions = [
     a2: 'terminal / bash',
     a3: 'for loops',
     a4: 'console.log()',
-    correct: 'console.log()'
+    correct: 'console.log()',
+    isCorrect: false
   },
   {
     question: 'Arrays in JavaScript can be used to store _____.',
@@ -44,7 +47,8 @@ let questions = [
     a2: 'other arrays',
     a3: 'booleans',
     a4: 'all of the above',
-    correct: 'all of the above'
+    correct: 'all of the above',
+    isCorrect: false
   },
   {
     question: 'String values must be enclosed within _____ when being assigned to variables.',
@@ -52,7 +56,8 @@ let questions = [
     a2: 'curly brackets',
     a3: 'quotes',
     a4: 'parentheses',
-    correct: 'parentheses'
+    correct: 'parentheses',
+    isCorrect: false
   },
   {
     question: 'Inside which HTML element do we put the JavaScript?',
@@ -60,7 +65,8 @@ let questions = [
     a2: '<scripting>',
     a3: '<script>',
     a4: '<javascript>',
-    correct: '<script>'
+    correct: '<script>',
+    isCorrect: false
   },
   {
     question: 'What is the correct JavaScript syntax to change the content of the HTML element: <p id="demo">This is a demonstration.</p>?',
@@ -68,7 +74,8 @@ let questions = [
     a2: 'document.getElementByName("p").innerHTML = "Hello World!";',
     a3: 'document.getElement("p").innerHTML = "Hello World!";',
     a4: 'document.getElementById("demo").innerHTML = "Hello World!";',
-    correct: 'document.getElementById("demo").innerHTML = "Hello World!";'
+    correct: 'document.getElementById("demo").innerHTML = "Hello World!";',
+    isCorrect: false
   },
   {
     question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
@@ -76,7 +83,8 @@ let questions = [
     a2: '<script name="xxx.js">',
     a3: '<script href="xxx.js">',
     a4: '<script link="xxx.js">',
-    correct: '<script src="xxx.js">'
+    correct: '<script src="xxx.js">',
+    isCorrect: false
   },
   {
     question: 'How do you write "Hello World" in an alert box?',
@@ -84,7 +92,8 @@ let questions = [
     a2: 'msg("Hello World");',
     a3: 'alertBox("Hello World");',
     a4: 'alertBox("Heloo World");',
-    correct: 'alert("Hello World");'
+    correct: 'alert("Hello World");',
+    isCorrect: false
   },
   {
     question: 'How do you call a function named "myFunction"?',
@@ -92,7 +101,8 @@ let questions = [
     a2: 'myFunction()',
     a3: 'call myFunction()',
     a4: 'myFunction().call',
-    correct: 'myFunction()'
+    correct: 'myFunction()',
+    isCorrect: false
   }
 ]
 
